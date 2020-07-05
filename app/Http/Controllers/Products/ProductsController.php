c4413c6a31e2811f3c5c7ebb977cad1c6280de18
Dmitry Novokreshennyh <d.novokreshennyh@key-g.com>
2020-07-05 15:48:28 +0300

    Create directory Models

--- app/Http/Controllers/Products/ProductsController.php
+++ app/Http/Controllers/Products/ProductsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Products;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Category;
+use App\Http\Models\Category;
 
 class ProductsController extends Controller
 {
