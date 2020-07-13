d18258bdbfd108ca9be4d6c8a92a854b95d391fe
Dmitry Novokreshennyh <d.novokreshennyh@key-g.com>
2020-07-13 17:47:04 +0300

    Remove boostrap js links

--- resources/views/snippets/insertions/header/sub-menu.blade.php
+++ resources/views/snippets/insertions/header/sub-menu.blade.php
@@ -1,5 +1,5 @@
 @foreach(config('shop.menu.list.header') as $item_menu)
-<li class="active">
-    <a href="{{ $item_menu['href'] }}">{{ $item_menu['title'] }} </a>
+<li>
+    <a href="{{ $item_menu['href'] }}">{{ $item_menu['title'] }}</a>
 </li>
 @endforeach
